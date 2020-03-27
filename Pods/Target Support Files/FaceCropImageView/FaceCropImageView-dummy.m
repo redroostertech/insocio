7a15358208d5d9d101b045605ad9350c8c27e94d
Michael Westbrooks <mwestbrooks@salesforce.com>
2020-03-27 18:02:38 -0400

    removed pods

--- Pods/Target Support Files/FaceCropImageView/FaceCropImageView-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_FaceCropImageView : NSObject
-@end
-@implementation PodsDummy_FaceCropImageView
-@end
--- Pods/Target Support Files/Freedom/Freedom-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Freedom : NSObject
-@end
-@implementation PodsDummy_Freedom
-@end
